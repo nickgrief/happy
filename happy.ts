c9e02e63341d45978716bd5192eff4346412e569
nickgrief <ngriev@gmail.com>
2022-10-26 00:35:51 +0300

    Initial commit

---
+++ happy.ts
@@ -0,0 +1,25 @@
+interface Quote {
+  q: string;
+  a: string;
+  h: string;
+}
+
+const OFFSET = Number(Deno.args[0]) || 2;
+
+const response = await fetch(
+  `https://zenquotes.io/api/random`,
+);
+
+const content: Quote[] = await response.json();
+
+const quote = content[0].q;
+const author = content[0].a;
+
+const quoteLength = quote.length;
+const authorLength = author.length;
+
+const spaceLength = quoteLength - authorLength + OFFSET;
+const spacing = " ".repeat(spaceLength);
+
+console.log(`"${quote}"\n`);
+console.log(spacing + author);
